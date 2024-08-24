99384ddc948817f0b0539c326cb36b81e1d1a99c
Rikesh <70803808+RikeshBohara@users.noreply.github.com>
2024-08-24 15:26:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //TMDB 
 
-const API_KEY = 'api_key=a23fd430d06a2aecfd5acaf20898f242';
+const API_KEY = 'YOUR API_KEY';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
